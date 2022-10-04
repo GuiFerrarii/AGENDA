aadd5af21e0492c57b03a7a23db420c7d61919d8
Guilherme Ferrari <105211172+GuiFerrarii@users.noreply.github.com>
2022-10-04 10:59:59 -0300

    Update detalhes-contatos.page.ts

--- app/pages/detalhes-contatos/detalhes-contatos.page.ts
+++ app/pages/detalhes-contatos/detalhes-contatos.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DadosContatosService } from 'src/app/service/dados-contatos.service';
+import { AlertController } from "@ionic/angular";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+
+
 
 
 @Component({
@@ -9,23 +15,44 @@ import { DadosContatosService } from 'src/app/service/dados-contatos.service';
   styleUrls: ['./detalhes-contatos.page.scss'],
 })
 export class DetalhesContatosPage implements OnInit {
+  cliente={}
+  clienteForm : FormGroup
+  
   
   public contatoSelecionado : any
   public modoDeEdicao = false
-
+  
   constructor(
     private route : ActivatedRoute,
-    private contatos : DadosContatosService
+    private contatos : DadosContatosService,
+    public alertController : AlertController,
+    private formBuilder : FormBuilder
+    
   ) { }
 
+  // enviar(){
+  //   if (this.clienteForm.invalid || this.clienteForm.pending){
+  //     return
+  //   }else{
+  //     console.log(this.cliente)
+  //   }
+  // }
+
+  
   IniciarEdicao(){
     this.modoDeEdicao = true
 
   }
 
+  enviar(){
+    if (this.clienteForm.invalid || this.clienteForm.pending){
+      return
+    }
+  }
   EncerrarEdicao(){
     const id : number = Number(this.route.snapshot.paramMap.get('id'))
     if (id > 0){
+      if (this.clienteForm.invalid)return
       this.modoDeEdicao = false
     } else {
       this.contatos.recebeDados(this.contatoSelecionado)
@@ -33,11 +60,39 @@ export class DetalhesContatosPage implements OnInit {
     }
   }
 
+  async showOptions() {
+    const alert = await this.alertController.create({
+      header: "Confirmar",
+      message: "Deseja mesmo excluir esse contato?",
+      buttons: [
+        {
+          text: "Voltar",
+          role: "cancel",
+          handler: () => {
+            console.log("Declined the offer");
+            
+            
+          },
+        },
+        {
+          text: "Sim",
+          handler: () => {
+            console.log("Accepted the offer");
+            this.deletarProduto()
+          },
+        },
+      ],
+    });
+  
+    await alert.present();
+  }
   deletarProduto(){
     this.contatos.deletaDados(this.contatoSelecionado)
   }
 
   ngOnInit() {
+   
+
     const id : number = Number(this.route.snapshot.paramMap.get('id'))
     if (id > 0){
       this.contatoSelecionado = this.contatos.enviarDadosId(id)
@@ -46,7 +101,18 @@ export class DetalhesContatosPage implements OnInit {
      this.modoDeEdicao = true 
     }
    
-  }
   
 
-}
+  this.clienteForm=this.formBuilder.group({
+    nome : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+    sobrenome : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+    email : ['', Validators.compose([Validators.required, Validators.email])],
+    telefone : ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(15)])],
+    tipo_tel : ['']
+    
+  })
+
+  }
+
+} 
+
